8f912c43a0ebb821da3b971815c30acfeffc8a8f
fengwutianya <949979029@qq.com>
2017-02-23 17:45:51 +0800

    行为层与结构层分离 退化

--- src/scripts/showPic.js
+++ src/scripts/showPic.js
@@ -12,3 +12,20 @@ function showPic(whichPic) {
     description.firstChild.nodeValue = titleText
 
 }
+
+function prepareGallery() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+
+    var imagegallery = document.getElementById("imagegallery");
+    var links = imagegallery.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        links[i].onclick = function() {
+            showPic(this);
+            return false;
+        }
+    }
+
+}
+window.onload = prepareGallery;
